df7af6c192c3e2eca4b17c11ad0ddb5fa40d4551
strawberryoolongtea <strawberrycream@kakao.com>
2021-10-30 19:10:45 +0900

    Start UI

--- app.js
+++ app.js
@@ -17,8 +17,18 @@ function getData(url) {
 function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
-
-  newsList.push('<ul>');
+  let template = `
+    <div class="container mx-auto p-4">
+      <h1>Hacker News</h1>
+      <ul>
+        {{__news_feed__}}
+      </ul>
+      <div>
+        <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+        <a href="#/page/{{__next_page__}}">다음 페이지</a>
+      </div>
+    </div>
+  `;
 
   for (let i = (store.currentPage - 1) * 10; i <  store.currentPage * 10; i++) {
     newsList.push(`
@@ -30,15 +40,13 @@ function newsFeed() {
     `);
   }
 
-  newsList.push('</ul>');
-  newsList.push(`
-    <div>
-      <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
-      <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
-    </div>
-  `);
+  template = template.replace('{{__news_feed__}}', newsList.join(''));
+  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
+  // template = template.replace('{{__next_page__}}', store.currentPage + 1);
+  // currentPage가 newsFeed의 갯수보다 작으면 1을 더하고 갯수와 같거나 크면 newsFeed의 갯수를 표시한다
+  template = template.replace('{{__next_page__}}', store.currentPage < newsFeed.length / 10 ? store.currentPage + 1 : newsFeed.length / 10);
 
-  container.innerHTML = newsList.join('');
+  container.innerHTML = template;
 }
 
 function newsContent() {
